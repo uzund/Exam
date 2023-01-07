4ff6b296e64312a093f390e1a910e559289d49bc
Davut Uzun <duzun@k1m38s01.42istanbul.com.tr>
2023-01-07 14:14:53 +0300

    Update...

---
+++ EXAM_RANK_02/ft_strcspn.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcspn.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 14:09:22 by duzun             #+#    #+#             */
+/*   Updated: 2023/01/07 14:14:15 by duzun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (reject[j])
+		{
+			if (s[i] == reject[j])
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+
+int	main(void)
+{
+	printf("org : %lu\n", strcspn("Davut", "Uzun"));
+	printf("emu : %lu\n", ft_strcspn("Davut", "Uzun"));
+	return (0);
+}
