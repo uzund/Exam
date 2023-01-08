fc9d505743a5224e4d3a96068a2a992c620786fa
Davut UZUN <duzun@10.0.2.15>
2023-01-08 17:12:24 +0300

    Update..

---
+++ EXAM_RANK_02/ft_strcmp.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 17:09:58 by duzun             #+#    #+#             */
+/*   Updated: 2023/01/08 17:10:11 by duzun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	char	*s1= "Davut";
+	char	*s2= "Davut Uzun";
+
+	printf("org :%d\n", strcmp(s1, s2));
+	printf("emu :%d\n", ft_strcmp(s1, s2));
+	return (0);
+}
